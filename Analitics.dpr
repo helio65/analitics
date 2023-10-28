7fe01a8473d8420b98542c4365281b0ef38ba7e6
Hélio José Almeida de Oliveira <62970607+helio65@users.noreply.github.com>
2023-10-28 20:31:27 -0300

    Add files via upload
    Arquivos iniciais

---
+++ Analitics.dpr
@@ -0,0 +1,14 @@
+program Analitics;
+
+uses
+  Vcl.Forms,
+  Main in 'Main.pas' {FrmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFrmMain, FrmMain);
+  Application.Run;
+end.
